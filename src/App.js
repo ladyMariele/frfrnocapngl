1159c95e21345cfa496ef5f862fbf595da1f7354
nellekocabrera <44804044+nellekocabrera@users.noreply.github.com>
2023-01-06 04:00:43 +0800

    some cool changes
    some cool changes ;)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Make mistakes now than later ;)</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
