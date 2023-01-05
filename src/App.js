02fc0777bb3032082ffb0bfe6578e4c5cc925717
Mariele <lullalayyy@gmail.com>
2023-01-06 04:15:59 +0800

    add emoticon

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Make mistakes now than later ;)</h1>
+        <p>>:O</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
