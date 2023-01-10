75a9cfb5e4082d703e6e9e003ad6325564c48451
nellekocabrera <44804044+nellekocabrera@users.noreply.github.com>
2023-01-11 00:59:43 +0800

    Add rejection page custom text

--- src/components/pages/RejectionPage.js
+++ src/components/pages/RejectionPage.js
@@ -1,8 +1,12 @@
-function RejectionPage()
+function RejectionPage(props)
 {
+
+    const rejectionText = "Sorry, but frfrnocapngl requires you to accept cookies";
+
     return (
         <div>
             <h1>Rejection Page</h1>
+            <p>{ props.customText ? customText : rejectionText }</p>
         </div>
     )
 }
