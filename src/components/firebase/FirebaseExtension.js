0e119959bae7d0b254926a060a47cc08756eda4e
nellekocabrera <44804044+nellekocabrera@users.noreply.github.com>
2023-01-11 02:35:36 +0800

    Firebase test

--- src/components/firebase/FirebaseExtension.js
+++ src/components/firebase/FirebaseExtension.js
@@ -1,3 +1,26 @@
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, onValue, set } from 'firebase/database'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCY84RYCuzRTyYHqUsgd9T9LiXAoaiC7OA",
+  authDomain: "sampleproject-d5085.firebaseapp.com",
+  databaseURL: "https://sampleproject-d5085-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sampleproject-d5085",
+  storageBucket: "sampleproject-d5085.appspot.com",
+  messagingSenderId: "98580911881",
+  appId: "1:98580911881:web:cc9f612c24693bceea65d8",
+  measurementId: "G-S21RDDQJ74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getDatabase()
+
 function GetMessages()
 {
 
@@ -20,5 +43,7 @@ function GetUser()
 
 function AddUser()
 {
-    
+    set(ref(db, 'users/' + '6'), {
+        name: 'mew'
+    })
 }
