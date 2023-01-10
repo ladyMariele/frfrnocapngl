868c8399fcb8dc99dce214a3c3366de2c7eab517
nellekocabrera <44804044+nellekocabrera@users.noreply.github.com>
2023-01-10 23:22:26 +0800

    Added Registration Page

---
+++ src/components/pages/RegistrationPage.js
@@ -0,0 +1,13 @@
+function RegistrationPage()
+{
+    return (
+
+        <div>
+            <h1> Registration Page </h1>
+            This is a registration page
+        </div>
+
+    )
+}
+
+export default RegistrationPage;
