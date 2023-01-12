1edca4ebe63dc0d3eaeeffc33f05697c25e09322
Mariele <102813740+ladyMariele@users.noreply.github.com>
2023-01-12 14:24:44 +0800

    😬 test api key

--- src/components/firebase/FirebaseExtension.js
+++ src/components/firebase/FirebaseExtension.js
@@ -7,7 +7,7 @@ import { getDatabase, ref, onValue, set, get, child } from 'firebase/database'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCY84RYCuzRTyYHqUsgd9T9LiXAoaiC7OA",
+  apiKey: "process.env.API_KEY",
   authDomain: "sampleproject-d5085.firebaseapp.com",
   databaseURL: "https://sampleproject-d5085-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "sampleproject-d5085",
